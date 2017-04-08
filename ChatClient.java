3b0983bb60448b01bc96bb2e186cc2221ea77060
jeherrera <jeherrera@cpp.edu>
2017-04-08 16:49:17 -0700

    Threaded Chat Client2

--- ChatClient.java
+++ ChatClient.java
@@ -10,58 +10,38 @@ public class ChatClient{
 		Socket socket = new Socket("codebank.xyz", 38001);
 		Scanner kb = new Scanner(System.in);
 		
-		try {
-
-
-			//BufferedReader br =  new BufferedReader(
-				//new InputStreamReader(socket.getInputStream(), "UTF-8"));
-			
+		try {			
 			PrintStream out = new PrintStream(socket.getOutputStream(), true, "UTF-8");
-			
 			ReaderThread reader = new ReaderThread(socket);
 			reader.start();
-
-
 			String username = "";
 			username = kb.nextLine();
 			out.printf(username + "%n");
-			
-			
 			String userinput = "";
-			
 			while (reader.isAlive()){         	
                 userinput = kb.nextLine();
                 out.println(userinput);   
-
 	        }
-
 		}catch (IOException e){
 
 		}
 	}
-
 	private static class ReaderThread extends Thread {
 		private Socket socket = null;
-
 		public ReaderThread(Socket socket){
 			this.socket = socket;
 		}
-
 		public void run() {
 			try{
-
 				BufferedReader br =  new BufferedReader(
 				new InputStreamReader(socket.getInputStream(), "UTF-8"));
-				
 				String userinput = " "; 
 				while ((userinput = br.readLine()) != null){
 	               	System.out.println(userinput);	
 	                if (userinput.equals("Name in use.")){
 	                	System.exit(0);
 	                }
-	                
 	            }
-
 			} catch(IOException e){
 				System.out.println("Abruptly Disconnected");
 			}
