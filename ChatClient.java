d7a46d84cfeb9a54a486f1d30db626ab23d32abb
jeherrera <jeherrera@cpp.edu>
2017-04-08 16:48:16 -0700

    Threaded Chat Client

--- ChatClient.java
+++ ChatClient.java
@@ -9,35 +9,62 @@ public class ChatClient{
 	public static void main(String[] args) throws Exception {
 		Socket socket = new Socket("codebank.xyz", 38001);
 		Scanner kb = new Scanner(System.in);
+		
 		try {
-			BufferedReader br =  new BufferedReader(
-				new InputStreamReader(socket.getInputStream(), "UTF-8"));
+
+
+			//BufferedReader br =  new BufferedReader(
+				//new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			
 			PrintStream out = new PrintStream(socket.getOutputStream(), true, "UTF-8");
 			
+			ReaderThread reader = new ReaderThread(socket);
+			reader.start();
+
+
 			String username = "";
 			username = kb.nextLine();
 			out.printf(username + "%n");
-		
+			
+			
 			String userinput = "";
 			
-			while (true){
-            	 	
-               	while (br.ready()){
-	                userinput = br.readLine();
-	                System.out.println(userinput);	
-	            }
-	            
-	            System.out.print(username + ": "); 
+			while (reader.isAlive()){         	
                 userinput = kb.nextLine();
-                out.printf(userinput + "%n");   
-                userinput = br.readLine();
-	            System.out.println(userinput);	
+                out.println(userinput);   
 
 	        }
-			
 
-		}catch (Exception e){
+		}catch (IOException e){
 
+		}
+	}
+
+	private static class ReaderThread extends Thread {
+		private Socket socket = null;
+
+		public ReaderThread(Socket socket){
+			this.socket = socket;
+		}
+
+		public void run() {
+			try{
+
+				BufferedReader br =  new BufferedReader(
+				new InputStreamReader(socket.getInputStream(), "UTF-8"));
+				
+				String userinput = " "; 
+				while ((userinput = br.readLine()) != null){
+	               	System.out.println(userinput);	
+	                if (userinput.equals("Name in use.")){
+	                	System.exit(0);
+	                }
+	                
+	            }
+
+			} catch(IOException e){
+				System.out.println("Abruptly Disconnected");
+			}
 		}
 	}
 }
